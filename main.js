d466f1cc303629c1d61253739c1e5d63b182f7e3
Schkullie <71150548+Schkullie@users.noreply.github.com>
2024-12-23 19:52:47 +0100

    Added up/download of json file

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 //const names = [];
-const previous = [];
+previous = [];
+result = []
 
 document.getElementById('random').disabled = true;
+document.getElementById('download').disabled = true;
 
 let btnAdd = document.getElementById('buttonplayername');
 let inputplayer = document.getElementById('playername');
@@ -10,7 +12,9 @@ let inputsave = document.getElementById('savename');
 btnAdd.addEventListener('click', () =>{
     if (inputplayer != "" && inputsave.value != "") {
     	//names.push(inputplayer.value);
-        previous.push({name:inputplayer.value, save:inputsave.value})
+        playerstr = encodeURI(inputplayer.value)
+        savestr = encodeURI(inputsave.value)
+        previous.push({name:playerstr, save: savestr})
         document.getElementById('old').innerHTML += '<tr><td>' + inputplayer.value + '</td><td>' + inputsave.value + '</td></tr>'
         document.getElementById('savename').value = ""
         document.getElementById('playername').value = ""
@@ -20,6 +24,33 @@ btnAdd.addEventListener('click', () =>{
       }
     }
 });
+
+let upload = document.getElementById('upload')
+upload.addEventListener('click', openDialog);
+function openDialog() {
+  document.getElementById('file').click();
+}
+
+(function(){
+function onChange(event) {
+    var reader = new FileReader();
+    reader.onload = onReaderLoad;
+    reader.readAsText(event.target.files[0]);
+}
+
+function onReaderLoad(event){
+    var obj = JSON.parse(event.target.result)
+    previous = previous.concat(obj)
+    console.log(previous)
+    document.getElementById('old').innerHTML = "<tr><th>Spieler</th><th>Speicherstand</th></tr>"
+    previous.forEach(element => document.getElementById('old').innerHTML += '<tr><td>' + decodeURI(element.name) + '</td><td>' + decodeURI(element.save) + '</td></tr>')
+    if (document.getElementById('random').disabled == true && previous.length > 2) {
+        document.getElementById('random').removeAttribute("disabled")
+        document.getElementById('random').setAttribute('class', 'btn btn-primary')
+    }
+}
+document.getElementById('file').addEventListener('change', onChange);
+}());
 
 function getRndInteger(min, max) {
     return (Math.floor(Math.pow(10,14)*Math.random()*Math.random())%(max-min+1))+min;
@@ -44,11 +75,29 @@ function randomise(PreviousNames) {
     }
 }
 
-
 let random = document.getElementById('random');
-
 random.addEventListener('click', () =>{
-	const result = randomise(previous)
+	result = randomise(previous)
     document.getElementById('output').innerHTML = "<tr><th>Spieler</th><th>Speicherstand</th></tr>"
-	result.forEach(element => document.getElementById('output').innerHTML += '<tr><td>' + element.name + '</td><td>' + element.save + '</td></tr>')
+	result.forEach(element => document.getElementById('output').innerHTML += '<tr><td>' + decodeURI(element.name) + '</td><td>' + decodeURI(element.save) + '</td></tr>')
+    document.getElementById('download').removeAttribute("disabled")
+    document.getElementById('download').setAttribute('class', 'btn btn-primary');
+});
+
+download = document.getElementById('download');
+download.addEventListener('click', () =>{
+    console.log(result)
+    const filename = 'Auslosung.json';
+    const jsonStr = JSON.stringify(result);
+
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(jsonStr));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
 });
